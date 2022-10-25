86464c061ffdf94adf269d972d66cbe9ba31f022
Michiel Dewachtere <michiel.dewachtere@gmail.com>
2022-10-25 10:14:09 +0200

    ReferenceScene is base scene

--- source/main.cpp
+++ source/main.cpp
@@ -45,7 +45,7 @@ int main(int argc, char* args[])
 	const auto pTimer = new Timer();
 	const auto pRenderer = new Renderer(pWindow);
 
-	const auto pScene = new Scene_W4_BunnyScene();
+	const auto pScene = new Scene_W4_ReferenceScene();
 	pScene->Initialize();
 
 	//Start loop
