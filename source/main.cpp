bc9326c00de173fbf9f9119b02d853f7dcee600e
Michiel Dewachtere <michiel.dewachtere@gmail.com>
2022-11-04 23:06:53 +0100

    "quality of life" Update

--- source/main.cpp
+++ source/main.cpp
@@ -45,7 +45,8 @@ int main(int argc, char* args[])
 	const auto pTimer = new Timer();
 	const auto pRenderer = new Renderer(pWindow);
 
-	const auto pScene = new Scene_W4_ReferenceScene();
+	//const auto pScene = new Scene_W4_ReferenceScene();
+	const auto pScene = new Scene_W4_BunnyScene();
 	pScene->Initialize();
 
 	//Start loop
